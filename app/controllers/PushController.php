97e71b990700b56ff679e25c7c39d3e0622b66bc
Forsaken <alexey2142@mail.ru>
2014-03-29 23:41:50 +1100

    push to android app

--- app/controllers/PushController.php
+++ app/controllers/PushController.php
@@ -2,6 +2,34 @@
 
 class PushController extends BaseController {
 
+	function sendPushNotificationToGCM($registation_ids, $message)
+	{
+		$url = 'https://android.googleapis.com/gcm/send';
+		$fields = array(
+			'registration_ids' => $registation_ids,
+			'data' => $message,
+		);
+		define("GOOGLE_API_KEY", "AIzaSyDqnS3844V6eACSFjQpFW1ngzakRmZ4pP4");   
+		$headers = array(
+			'Authorization: key=' . GOOGLE_API_KEY,
+			'Content-Type: application/json'
+		);
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+		$result = curl_exec($ch);
+		if ($result === FALSE) {
+			die('Curl failed: ' . curl_error($ch));
+		}
+		curl_close($ch);
+		return $result;
+	}
+
 	public function test()
 	{
 		$client = new Google_Client();
@@ -11,15 +39,9 @@ class PushController extends BaseController {
 		$client->setClientSecret('BmKlhDHClYCS8g-PKTZ_uelz');
 		$client->setRedirectUri('/');
 
-		echo 'success auth';
-	}
-
-	public function auth()
-	{
-		if (isset($_GET['code'])) {
-			$client->authenticate();
-			$_SESSION['token'] = $client->getAccessToken();
-			header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
-		}
+		echo $this->sendPushNotificationToGCM(
+			array('APA91bGsb0nWZaQmSu9C6G2xlkZTgPBmNcRxtdoFkd7uxjcqcsy97kUU42uEZync_j9cM_VS96bJdLP0YSd7iQZAwjit58zs3KzV-FCpHdTxO4V4dD_HoFM8wKN3895zLX6xhOJTigkClDDWWB_2BhA0_RWK6IRQMg'), 
+			array('message' => 'hello, android')
+		);
 	}
 }
