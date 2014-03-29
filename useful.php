b5059309cf3d0ea1d0109daecc01fd7d6409c46c
jhonyxakep <jhonyxakep@qip.ru>
2014-03-29 23:23:25 +1100

    Update useful.php
    Функция 100% запроса контактов, парсинг имейлов

--- useful.php
+++ useful.php
@@ -39,22 +39,24 @@ return $Razdely;
 }
 
 
+
 //Логин на фарпосте. Нужно вызывать перед запросом контактных данных. Создает файл cookies.txt
 function FarPostLogin($login="Hackaton",$password="EHtvRXABI0",$url='https://vladivostok.farpost.ru/sign?return=%2F')
 {
  if( $curl = curl_init() ) 
- {
+ { 
+    //unlink("cookies.txt");
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
     curl_setopt($curl, CURLOPT_COOKIEFILE, "cookies.txt"); 
     curl_setopt($curl, CURLOPT_COOKIEJAR, "cookies.txt");
-    curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0"); 
+    curl_setopt($curl, CURLOPT_USERAGENT, "Opera/9.80 (X11; Linux x86_64; Edition Linux Mint) Presto/2.12.388 Version/12.16"); 
     curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_POSTFIELDS, "radio=sign&sign=".$login."&password=".$password);
     
     $Page = curl_exec($curl);
     curl_close($curl);
-    //var_dump($Page);
+ //   var_dump($Page);
     if(strlen($Page)!=0) return false;
     return true;
  } 
@@ -67,11 +69,12 @@ function GetContacts($url)
 {
  if( $curl = curl_init() ) 
  {
+    
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
     curl_setopt($curl, CURLOPT_COOKIEFILE, "cookies.txt"); 
     curl_setopt($curl, CURLOPT_COOKIEJAR, "cookies.txt");
-    curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0");    
+    curl_setopt($curl, CURLOPT_USERAGENT, "Opera/9.80 (X11; Linux x86_64; Edition Linux Mint) Presto/2.12.388 Version/12.16");    
     $Page = curl_exec($curl);
  
     $URL=GetBetween($Page,'class="bigbutton viewAjaxContacts" href="','">');
@@ -79,10 +82,15 @@ function GetContacts($url)
     curl_setopt($curl, CURLOPT_REFERER, $url);
     //curl_setopt($curl,CURLOPT_FOLLOWLOCATION,1);
    
+   
+    curl_setopt($curl, CURLOPT_URL, "http://vladivostok.farpost.ru/");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+    $Page = curl_exec($curl);
     
     curl_setopt($curl, CURLOPT_URL, "http://vladivostok.farpost.ru/".$URL."?ajax=1");
     curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
     $Page = curl_exec($curl);
+    
     curl_close($curl);
 
  } 
@@ -105,9 +113,34 @@ $phones=array();
     $temp=ExtractPhone($Page);
     if($temp=="") break;
     $Page=substr($Page, strpos($Page,$temp)+strlen($temp));
+    $temp=str_replace(array('(','-',')',' '), "", $temp);
+    $temp=str_replace('+7', "8", $temp);
     $phones[]=$temp;
     }
     return $phones;
+}
+
+
+
+//Берет первый найденный имейл
+function ExtractMail($Page)
+{
+return GetBetween($Page,'mailto:','?');
+}
+
+//Отдает массив с имейлами
+function ExtractMails($Page)
+{
+$mailes=array();
+    $temp="temp";	
+    while($temp!='')
+    {
+    $temp=ExtractMail($Page);
+    if($temp=="") break;
+    $Page=substr($Page, strpos($Page,$temp)+strlen($temp));
+    $mailes[]=$temp;
+    }
+    return $mailes;
 }
 
 
@@ -180,13 +213,38 @@ function GetPosts($url)
 return $posts;
 }
 
+//мега костыль, получает контактные данные инфа 146%
+function TryGetContacts($url)
+{
 if(FarPostLogin())
 {
-$Page=GetContacts("http://vladivostok.farpost.ru/samye-shikarnye-limuziny-na-dalnem-vostoke-infiniti-chrysler-vipavto-20112047.html");
+
+	while(1)
+	{	
+	$Page=GetContacts($url);
+	if(GetBetween($Page,'class="phone">',"span")!='')
+		{
+			unlink("cookies.txt");
+			FarPostLogin();
+			FarPostLogin();
+			sleep(1);
+		}
+	else
+	break;
+
+	}
+	
+}
+return $Page;
+}
+
+
+	
+$Page=TryGetContacts("http://vladivostok.farpost.ru/dostupnoe-stroitelstvo-iz-sendvicha-individualnye-proekty-svai-12446791.html");
 var_dump($Page);
 var_dump(ExtractPhones($Page));
+var_dump(ExtractMails($Page));
 
-}
 //var_dump(GetImagesUrls("http://vladivostok.farpost.ru/samye-shikarnye-limuziny-na-dalnem-vostoke-infiniti-chrysler-vipavto-20112047.html"));
 
 //var_dump(GetPosts("http://vladivostok.farpost.ru/service/celebrate/?page=3"));
