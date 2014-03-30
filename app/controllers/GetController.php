a44a3c8bcdc51699e1ca658c37ce8d5a058d7838
GotFly <gotovets.mail@gmail.com>
2014-03-30 16:21:51 +1100

    Добавление проверок

--- app/controllers/GetController.php
+++ app/controllers/GetController.php
@@ -4,13 +4,20 @@ class GetController extends BaseController {
 
 	public function get()
 	{
-		if (strlen(Input::get('time')) <= 0)
+		if(!Request::isJson())
+		{
+			return Response::json(array('message' => 'Bad headers: not json'));
+		}
+
+		$input = Input::get();
+
+		if (strlen($input['time']) <= 0)
 		{
 			return Response::json(array('message' => 'Not set time'));
 		}
 
 		$ids = array();
-		foreach (User::where('devise_id', Input::get('register_id'))->first()->category as $category)
+		foreach (User::where('devise_id', $input['register_id'])->first()->category as $category)
 		{
 			$ids[] = $category->category_id;
 		}
@@ -22,7 +29,7 @@ class GetController extends BaseController {
 
 		return Response::json(array(
 			'time' => Carbon\Carbon::now()->toDateTimeString(),
-			'isems' => Stack::whereIn('category_id', $ids)->where('created_at', '<=', Input::get('time'))->get()->toArray()
+			'isems' => Stack::whereIn('category_id', $ids)->where('created_at', '<=', $input['time'])->get()->toArray()
 		));
 	}
 }
