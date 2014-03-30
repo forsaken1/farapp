4091bc0326bb73d56f0c317f2bfee8a5497d2845
Forsaken <alexey2142@mail.ru>
2014-03-30 13:28:20 +1100

    model rel + heroku deploy

--- app/models/UserCategory.php
+++ app/models/UserCategory.php
@@ -3,4 +3,14 @@
 class UserCategory extends Eloquent {
 	protected $guarded = array();
 	protected $table = 'user_category';
+
+	public function user()
+	{
+		return $this->belongsTo('User', 'user_id');
+	}
+
+	public function category()
+	{
+		return $this->belongsTo('Category', 'category_id');
+	}
 }
