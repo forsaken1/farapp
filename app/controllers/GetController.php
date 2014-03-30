60d3ea97973b57e4e441803f550cb70e2c19c1b7
GotFly <gotovets.mail@gmail.com>
2014-03-30 16:16:18 +1100

    Bugfix

--- app/controllers/GetController.php
+++ app/controllers/GetController.php
@@ -5,7 +5,7 @@ class GetController extends BaseController {
 	public function get()
 	{
 		$ids = array();
-		foreach (User::where('devise_id', 'test')->first()->category as $category)
+		foreach (User::where('devise_id', Input::get('register_id'))->first()->category as $category)
 		{
 			$ids[] = $category->category_id;
 		}
@@ -14,10 +14,10 @@ class GetController extends BaseController {
 		{
 			return Response::json(array('message' => 'User not select categories'));
 		}
-
+		
 		return Response::json(array(
-			'time' => Carbon\Carbon::now(),
-			'isems' => Stack::whereIn('category_id', $ids)->where('created_at', '<=', '0000-00-00 00:00:00')->get()->toArray()
+			'time' => Carbon\Carbon::now()->toDateTimeString(),
+			'isems' => Stack::whereIn('category_id', $ids)->where('created_at', '<=', Input::get('time'))->get()->toArray()
 		));
 	}
 }
